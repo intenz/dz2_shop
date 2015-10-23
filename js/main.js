3bc7ed7fa87d1fa5610ff89e9a72a4370eeafe32
Viktor <_intenz_@ukr.net>
2015-10-23 14:54:55 +0300

    edits list

--- js/main.js
+++ js/main.js
@@ -21,58 +21,81 @@ jQuery(document).ready(function($) {
 				item.stop(true, true).removeClass('active');
 			}
 	}); // -> end accordeon
-	$(function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 30000,
-      values: [ 100, 13000 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val(ui.values[ 0 ]);
-        $( "#amount1" ).val(ui.values[ 1 ]);
-      }
-    });
-    $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ));
-    $( "#amount1" ).val($( "#slider-range" ).slider( "values", 1 ));
 
-    $("input#amount").change(function(){
-    	var value1=$("input#amount").val();
-    	var value2=$("input#amount1").val();
+    $('.slid_show__link').on('click', function(e){
+        e.preventDefault();
 
-    		if(parseInt(value1) > parseInt(value2)){
-    			value1 = value2;
-    			$("input#amount").val(value1);
-    		}
-    		$("#slider-range").slider("values",0,value1);
+        var
+            $this = $(this),
+            item = $this.closest('.slid_show__item'),
+            container = $this.closest('.phone__item'),
+            display = container.find('.phone__link'),
+            path = item.find('img').attr('src'),
+            duration = 300;
+
+        if(!item.hasClass('active')) {
+            item.addClass('active').siblings().removeClass('active');
+
+            display.find('img').fadeOut(duration,function(){
+                $(this).attr('src', path).fadeIn(duration);
+            });
+        }
     });
 
-     $("input#amount1").change(function(){
-    	var value1=$("input#amount").val();
-    	var value2=$("input#amount1").val();
 
-    		if(parseInt(value1) > parseInt(value2)){
-    			value2 = value1;
-    			$("input#amount1").val(value2);
-    		}
-    		$("#slider-range").slider("values",1,value2);
 
-    	jQuery("#amount, #ampunt1").keypress(function(event){
-    		var key,keyChar;
-    		if (!event) var event = window.event;
+    $(function() {
+      $( "#slider-range" ).slider({
+          range: true,
+          min: 0,
+          max: 30000,
+          values: [ 100, 13000 ],
+          slide: function( event, ui ) {
+            $( "#amount" ).val(ui.values[ 0 ]);
+            $( "#amount1" ).val(ui.values[ 1 ]);
+          }
+        });
+        $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ));
+        $( "#amount1" ).val($( "#slider-range" ).slider( "values", 1 ));
 
-    		if (event.keyCode) key = event.keyCode;
-    		else if(event.which) key = event.which;
+        $("input#amount").change(function(){
+        	var value1=$("input#amount").val();
+        	var value2=$("input#amount1").val();
 
-    		if(key==null || key==0 || key==8 || key==13 || key==46 || key==37 || key==39)
-    			return true;
-    		keyChar=String.fromCharCode(key);
+        		if(parseInt(value1) > parseInt(value2)){
+        			value1 = value2;
+        			$("input#amount").val(value1);
+        		}
+        		$("#slider-range").slider("values",0,value1);
+        });
 
-    		if(!/\d/.test(keyChar)) return false;
-    		
-    	});
-    });
+         $("input#amount1").change(function(){
+        	var value1=$("input#amount").val();
+        	var value2=$("input#amount1").val();
 
-  }); // -> end slider UI
+        		if(parseInt(value1) > parseInt(value2)){
+        			value2 = value1;
+        			$("input#amount1").val(value2);
+        		}
+        		$("#slider-range").slider("values",1,value2);
+
+        	jQuery("#amount, #ampunt1").keypress(function(event){
+        		var key,keyChar;
+        		if (!event) var event = window.event;
+
+        		if (event.keyCode) key = event.keyCode;
+        		else if(event.which) key = event.which;
+
+        		if(key==null || key==0 || key==8 || key==13 || key==46 || key==37 || key==39)
+        			return true;
+        		keyChar=String.fromCharCode(key);
+
+        		if(!/\d/.test(keyChar)) return false;
+        		
+        	});
+      });
+
+    }); // -> end slider UI
 
 }); // - > ready_end
 
